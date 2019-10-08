e7e83d2a516cc023e5f24526bac74e4632147301
haoer214 <770545775@qq.com>
2019-10-08 21:32:26 +0800

    2019-10-08

---
+++ src/array/a015.java
@@ -0,0 +1,40 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// 15. 三数之和
+// 「双指针」时间O(n2)
+public class a015 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        if (nums == null || nums.length < 3) {
+            return list;
+        }
+        int len = nums.length;
+        Arrays.sort(nums);
+        for (int k = 0; k < len-2; k++) {
+            if (nums[k] > 0) {
+                break;
+            }
+            if (k > 0 && nums[k] == nums[k - 1]) {
+                continue;
+            }
+            int i = k + 1, j = len - 1;
+            while (i < j) {
+                int sum = nums[k] + nums[i] + nums[j];
+                if (sum < 0) {
+                    while (i < j && nums[i] == nums[++i]);
+                } else if (sum > 0) {
+                    while (i < j && nums[j] == nums[--j]);
+                } else {
+                    list.add(Arrays.asList(nums[k], nums[i], nums[j]));
+                    while (i < j && nums[i] == nums[++i]);
+                    while (i < j && nums[j] == nums[--j]);
+                }
+            }
+        }
+        return list;
+    }
+}
