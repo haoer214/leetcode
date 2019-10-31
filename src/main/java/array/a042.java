dc464b3c4ae2ce17272a0cdeba14cd53abb7a6bb
haoer214 <770545775@qq.com>
2019-10-31 22:09:09 +0800

    191031

--- src/main/java/array/a042.java
+++ src/main/java/array/a042.java
@@ -6,16 +6,38 @@ package array;
 public class a042 {
     // 「双指针」
     public int trap(int[] height) {
+        if (height == null || height.length < 3)
+            return 0;
         int len = height.length;
         int maxWater = 0;
-        for (int i = 0; i < len; i++) {
-            for (int j = i+1; j < len; j++) {
-                if (height[j] >= height[i]) {
-                    i = j;
-                    break;
-                }
-                maxWater += height[i] - height[j];
+        int tmpWater = 0;
+        // 从左往右遍历
+        int left = 0, i = left+1;
+        while (i < len) {
+            if (height[i] >= height[left]) {
+                maxWater += tmpWater;
+                tmpWater = 0;
+                left = i++;
+            } else {
+                tmpWater += height[left] - height[i++];
             }
+        }
+        // 找到最高的柱子
+        int top = left;
+        if (height[len-1] < height[top]) {
+            tmpWater = 0;
+            // 从右往左遍历
+            int right = len-1, j = right-1;
+            while (j > top) {
+                if (height[j] >= height[right]) {
+                    maxWater += tmpWater;
+                    tmpWater = 0;
+                    right = j--;
+                } else {
+                    tmpWater += height[right] - height[j--];
+                }
+            }
+            maxWater += tmpWater;
         }
         return maxWater;
     }
---
+++ src/test/java/array/a042Test.java
@@ -0,0 +1,14 @@
+package array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class a042Test {
+
+    @Test
+    public void trap() {
+        System.out.println(new a042().trap(new int[]{0,1,0,2,1,0,1,3,2,1,2,1}));
+    }
+}
